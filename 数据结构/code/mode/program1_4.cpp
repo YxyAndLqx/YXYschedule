c2965dcf5b7eb796aa5b15f08862fa3766ac161f
DragonSlayerKF <1652145909@qq.com>
2020-04-19 21:52:53 +0800

    2020-4-19

---
+++ 数据结构/code/mode/program1_4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+void main1()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << "a: " << a << "b: " << b << endl;
+}
---
+++ 数据结构/code/mode/program1_5.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+void main()
+{
+	ifstream inFile;
+	ofstream outFlie;
+	outFlie.open("..//iofiles//out.txt", ios::out);
+	char univ[] = "Tsinghua", name[10];
+	int course = 2401, number;
+	outFlie << univ << endl;
+	outFlie << course << endl;
+	inFile.open("..//iofiles//out.txt", ios::in | ios::_Nocreate);
+	if (!inFile) {
+		cerr << "不能打开 iofiles\out.txt" << endl;
+		exit(1);
+	}
+	char c;
+	inFile >> name >> c >> number;
+	outFlie << "name: " << name << endl;
+	outFlie << "number: " << number << endl;
+}
